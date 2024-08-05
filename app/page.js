57b3eb249064f156d376fa3df59b04a77b66e1e8
Ghulam Nabi Ahmed <Intern.Ghulam@runwei.org>
2024-08-05 13:07:15 -0400

    Basic pantry tracker app ready

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 'use client';
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { collection, deleteDoc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 import { firestore } from "@/firebase.js"; // Import correctly from firebase.js
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Modal, Stack, TextField, Typography } from '@mui/material';
 import { async } from "@firebase/util";
 
 export default function Home() {
@@ -66,9 +66,53 @@ export default function Home() {
 
 
   return (
-    <Box width="100vw" height = "100vh" display = "flex" justifyContent= "center" >
-      <Typography variant="h1" > Inventory Management </Typography>
-      
+    <Box width="100vw" height = "100vh" display = "flex" justifyContent= "center" alignItems="center" gap={2} flexDirection="column">
+      <Modal open = {open} onClose = {handleClose}>
+        <Box position= "absolute" top="50%" left ="50%" width = {400} 
+        bgcolor="white" border="2px solid #000" boxShadow={24}
+        p = {4} display="flex" flexDirection="column" gap={3} sx={{ transform: "translate(-50%, -50%)"}}>
+          <Typography variant="h6">Add Item </Typography>
+          <Stack width = "100%" direction="row" spacing={2}>
+            <TextField variant="outlined" fullWidth value={itemName} onChange={(e) => {setItemName(e.target.value)}}/>
+            <Button variant="outlined" onClick={ () => {
+               addItem(itemName) 
+               setItemName('') 
+               handleClose()}}>Add </Button>
+
+          </Stack>
+        </Box>
+
+      </Modal>
+      <Button variant="contained" onClick={()=>{
+        handleOpen()
+      }}>
+        Add New Item
+      </Button>
+      <Box border="1px solid #333" >
+        <Box width="800px" height="100px" bgcolor="#ADD8E6" alignItems="center" justifyContent="center" display="flex" >
+          <Typography variant="h2" color="#333" >Inventory Items</Typography>
+
+        </Box>
+       
+      <Stack width="800px" height="300px" spacing={2} overflow="auto">
+        {
+          inventory.map(({name, quantity}) =>(
+            <Box key={name} width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5}>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant="h3" color="#333" textAlign="center">
+                {quantity}
+              </Typography>
+              <Button variant="contained" onClick={()=>{
+                removeItem(name)
+              }}>Remove
+              </Button>
+            </Box>
+          ))
+        }
+      </Stack>
+      </Box>
     </Box>
   );
 }
